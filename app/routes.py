9d06f64e98c4556d7eaf2e352a607f7f34c08d04
Igor <igor.zamaraev@gcorelabs.com>
2021-05-21 19:11:29 +0500

    Обработка переменной части URL и аргументов GET запроса.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app import users as u
 
@@ -6,14 +6,29 @@ from app import users as u
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    text = 'Test endpoint.'
+    return render_template('index.html', text=text)
 
 
 @app.route('/users')
-def users():
-    return render_template('users.html', title='Users list', users=u.users_dict)
+@app.route('/users/<username>')
+def users(username=None):
+    temp_list = list()
+    if request.args.get('username'):
+        username = request.args.get('username')
+    if request.args.get('department'):
+        depart = request.args.get('department')
+    if username:
+        for user in u.users_list:
+            if user['username'] == username:
+                temp_list.append(user)
+    else:
+        temp_list = u.users_list
+    return render_template('users.html', title='Users list', users=temp_list)
 
 
 @app.route('/department')
 def department():
+    if request.args.get('name'):
+        depart = request.args.get('name')
     return render_template('departments.html')
