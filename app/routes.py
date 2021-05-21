26e5ec06044b08728257d3fc8810b2153d836e99
Igor <igor.zamaraev@gcorelabs.com>
2021-05-21 16:00:30 +0500

    Добавил 3 чекпоинта и шаблоны для них.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
+from app import users as u
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/users')
+def users():
+    return render_template('users.html', title='Users list', users=u.users_dict)
+
+
+@app.route('/department')
+def department():
+    return render_template('departments.html')
---
+++ app/users.py
@@ -0,0 +1,13 @@
+users_dict = [{'id': 1,
+               'username': 'user1',
+               'email': 'user1@test.com',
+               'department': 'frontend',
+               'date_joined': 'date'
+               },
+              {'id': 2,
+               'username': 'user2',
+               'email': 'user2@test.com',
+               'department': 'backend',
+               'date_joined': 'date'
+               },
+              ]
