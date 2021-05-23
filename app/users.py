57b271ef36e591e4ea45a6a38d1c11be7c105924
Beellzebub <54277370+Beellzebub@users.noreply.github.com>
2021-05-23 22:26:18 +0500

    Добавлены данные.

--- app/users.py
+++ app/users.py
@@ -1,13 +1,34 @@
+"""
+List of dictionary for emulated DB.
+"""
 users_list = [{'id': 1,
-               'username': 'user1',
+               'username': 'Tony',
                'email': 'user1@test.com',
-               'department': 'frontend',
-               'date_joined': 'date'
+               'department': 'fullstack',
+               'date_joined': '2020-01-20T09:00:00'
                },
               {'id': 2,
-               'username': 'user2',
+               'username': 'Steve',
                'email': 'user2@test.com',
+               'department': 'frontend',
+               'date_joined': '2020-01-20T09:00:00'
+               },
+              {'id': 3,
+               'username': 'Natasha',
+               'email': 'user3@test.com',
                'department': 'backend',
-               'date_joined': 'date'
+               'date_joined': '2020-01-20T09:00:00'
+               },
+              {'id': 4,
+               'username': 'Bruce',
+               'email': 'user4@test.com',
+               'department': 'frontend',
+               'date_joined': '2020-03-20T09:00:00'
+               },
+              {'id': 5,
+               'username': 'Stephen',
+               'email': 'user5@test.com',
+               'department': 'automation',
+               'date_joined': '2020-05-20T09:00:00'
                },
               ]
