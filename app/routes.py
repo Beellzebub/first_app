17afb999fb6c74ec5f9457983f6c966738799591
Beellzebub <54277370+Beellzebub@users.noreply.github.com>
2021-05-23 22:27:58 +0500

    Дописана логика фильтров (GET-параметры), согласно техзаданию.

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,63 @@
 from flask import render_template, request
 from app import app
-from app import users as u
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    text = 'Test endpoint.'
-    return render_template('index.html', text=text)
+from app.users import users_list
 
 
 @app.route('/users')
-@app.route('/users/<username>')
-def users(username=None):
+def users():
+    """
+    Create endpoint /users contains list of users.
+    Filters (can used together):
+        -username (partial or complete match).
+        -department (complete match).
+    If matches not found - show empty page.
+    :return: users.html
+    """
     temp_list = list()
-    if request.args.get('username'):
-        username = request.args.get('username')
-    if request.args.get('department'):
-        depart = request.args.get('department')
-    if username:
-        for user in u.users_list:
-            if user['username'] == username:
+    username = request.args.get('username')
+    depart_name = request.args.get('department')
+
+    for user in users_list:
+        if username and depart_name:
+            if username.lower() in user['username'] and depart_name == user['department']:
                 temp_list.append(user)
-    else:
-        temp_list = u.users_list
+        elif username:
+            if username in user['username']:
+                temp_list.append(user)
+        elif depart_name:
+            if depart_name == user['department']:
+                temp_list.append(user)
+        else:
+            temp_list.append(user)
+
     return render_template('users.html', title='Users list', users=temp_list)
 
 
 @app.route('/department')
 def department():
-    if request.args.get('name'):
-        depart = request.args.get('name')
-    return render_template('departments.html')
+    """
+    Create endpoint /department contains list of departments.
+    Filter:
+        -name (partial or complete match).
+    If matches not found - show empty page.
+    :return: departments.html
+    """
+    temp_list = list()
+    depart_name = request.args.get('name')
+
+    for user in users_list:
+        if depart_name:
+            if depart_name in user['department']:
+                for temp_user in temp_list:
+                    if temp_user['department'] == user['department']:
+                        break
+                else:
+                    temp_list.append(user)
+        else:
+            for temp_user in temp_list:
+                if temp_user['department'] == user['department']:
+                    break
+            else:
+                temp_list.append(user)
+
+    return render_template('departments.html', title='Departments list', users=temp_list)
