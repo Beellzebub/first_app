c03f7f2f738da0fa13353d830982b8f6832cf579
Beellzebub <54277370+Beellzebub@users.noreply.github.com>
2021-05-25 00:01:47 +0500

    Реализована API.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from app import app
 from app.users import users_list
 
 
-@app.route('/users')
+@app.route('/users/')
 def users():
     """
     Create endpoint /users contains list of users.
@@ -13,27 +13,25 @@ def users():
     If matches not found - show empty page.
     :return: users.html
     """
-    temp_list = list()
     username = request.args.get('username')
     depart_name = request.args.get('department')
-
-    for user in users_list:
-        if username and depart_name:
-            if username.lower() in user['username'] and depart_name == user['department']:
-                temp_list.append(user)
-        elif username:
-            if username in user['username']:
-                temp_list.append(user)
-        elif depart_name:
-            if depart_name == user['department']:
-                temp_list.append(user)
-        else:
-            temp_list.append(user)
-
-    return render_template('users.html', title='Users list', users=temp_list)
+    final_list = users_filter(username, depart_name)
+    return render_template('users.html', title='Users list', users=final_list)
 
 
-@app.route('/department')
+@app.route('/users/api/')
+def users_api():
+    """
+
+    :return: json
+    """
+    username = request.args.get('username')
+    depart_name = request.args.get('department')
+    final_list = users_filter(username, depart_name)
+    return jsonify(final_list)
+
+
+@app.route('/department/')
 def department():
     """
     Create endpoint /department contains list of departments.
@@ -42,9 +40,29 @@ def department():
     If matches not found - show empty page.
     :return: departments.html
     """
-    temp_list = list()
     depart_name = request.args.get('name')
+    final_list = depart_filter(depart_name)
+    return render_template('departments.html', title='Departments list', users=final_list)
 
+
+@app.route('/department/api/')
+def department_api():
+    """
+
+    :return:
+    """
+    depart_name = request.args.get('name')
+    final_list = depart_filter(depart_name)
+    return jsonify(final_list)
+
+
+def depart_filter(depart_name):
+    """
+
+    :param depart_name:
+    :return:
+    """
+    temp_list = list()
     for user in users_list:
         if depart_name:
             if depart_name in user['department']:
@@ -59,5 +77,27 @@ def department():
                     break
             else:
                 temp_list.append(user)
+    return temp_list
 
-    return render_template('departments.html', title='Departments list', users=temp_list)
+
+def users_filter(username=None, depart_name=None):
+    """
+
+    :param username:
+    :param depart_name:
+    :return:
+    """
+    temp_list = list()
+    for user in users_list:
+        if username and depart_name:
+            if username.lower() in user['username'] and depart_name == user['department']:
+                temp_list.append(user)
+        elif username:
+            if username in user['username']:
+                temp_list.append(user)
+        elif depart_name:
+            if depart_name == user['department']:
+                temp_list.append(user)
+        else:
+            temp_list.append(user)
+    return temp_list
