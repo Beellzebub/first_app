39c2894a099437c3563761de2bb7d24e55907aca
Igor <igor.zamaraev@gcorelabs.com>
2021-05-25 18:54:22 +0500

    Написаны фикстуры и часть тестов.
    request_to_server() - создает класс клиента и принимает ендпоинт для запроса.
    request_to_db() - эмулирует подключение к БД.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,19 @@
 import pytest
-import requests
+from app import app
 from app.users import users_list
 
 
 @pytest.fixture()
-def upload_data():
-    r = requests.get('http://127.0.0.1:5000/users')
-    return r.text
+def request_to_server():
+
+    def _set_route(route):
+        client = app.test_client()
+        res = client.get(route)
+        return res.get_json()
+
+    return _set_route
+
+
+@pytest.fixture()
+def request_to_db():
+    return users_list
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,28 +1,21 @@
-
-
 class TestUsers:
-    def test_without_param(self):
-        pass
+    def test_without_param(self, request_to_server, request_to_db):
+        assert len(request_to_server('/api/users/')) == len(request_to_db)
 
-    def test_param_username(self):
-        pass
+    def test_param_username(self, request_to_server, request_to_db):
+        assert request_to_server('/api/users/?username=To') == [item for item in request_to_db
+                                                                if item['username'] == 'Tony']
 
-    def test_param_departments(self):
-        pass
+    def test_param_departments(self, request_to_server, request_to_db):
+        assert True
 
-    def test_double_param(self):
-        pass
+    def test_double_param(self, request_to_server, request_to_db):
+        assert True
 
 
 class TestDepartment:
-    def test_without_param(self):
-        pass
+    def test_without_param(self, request_to_server, request_to_db):
+        assert True
 
-    def test_param_name(self):
-        pass
-
-
-class TestFirst:
-    def test_any(self, upload_data):
-        print(upload_data)
+    def test_param_name(self, request_to_server, request_to_db):
         assert True
