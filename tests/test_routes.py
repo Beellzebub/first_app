04ba6644516c3b3ea339f6f5c1b1eeef7678309b
Igor <igor.zamaraev@gcorelabs.com>
2021-05-31 14:45:25 +0500

    Дописаны тесты users. Добавлена параметризация в тесты routes.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class TestUsers:
     def test_get_request(self, request_to_server):
         assert request_to_server('/api/users/').status_code == 200
@@ -6,24 +9,24 @@ class TestUsers:
     def test_without_param(self, request_to_server, request_to_db):
         assert len(request_to_server('/api/users/').get_json()) == len(request_to_db)
 
-    def test_param_username(self, request_to_server, request_to_db):
-        user = 'Tony'
+    @pytest.mark.parametrize('user', ['Tony', 'Bruce'])
+    def test_param_username(self, request_to_server, request_to_db, user):
         req = f'/api/users/?username={user}'
         assert request_to_server(req).get_json() == [item for item in request_to_db
-                                                     if item['username'] == 'Tony']
+                                                     if item['username'] == f'{user}']
 
-    def test_param_departments(self, request_to_server, request_to_db):
-        department = 'frontend'
-        req = f'/api/users/?department={department}'
+    @pytest.mark.parametrize('dep', ['frontend', 'fullstack'])
+    def test_param_departments(self, request_to_server, request_to_db, dep):
+        req = f'/api/users/?department={dep}'
         assert request_to_server(req).get_json() == [item for item in request_to_db
-                                                     if item['department'] == 'frontend']
+                                                     if item['department'] == f'{dep}']
 
-    def test_double_param(self, request_to_server, request_to_db):
-        user = 'Bruce'
-        department = 'frontend'
-        req = f'/api/users/?username={user}&department={department}'
+    @pytest.mark.parametrize('user, dep', [('Tony', 'fullstack'), ('Bruce', 'frontend')])
+    def test_double_param(self, request_to_server, request_to_db, user, dep):
+        req = f'/api/users/?username={user}&department={dep}'
         assert request_to_server(req).get_json() == [item for item in request_to_db
-                                                     if item['department'] == 'frontend' and item['username'] == 'Bruce']
+                                                     if
+                                                     item['department'] == f'{dep}' and item['username'] == f'{user}']
 
 
 class TestDepartment:
@@ -38,11 +41,11 @@ class TestDepartment:
                 depart_list.append(item['department'])
         assert len(request_to_server('/api/department/').get_json()) == len(depart_list)
 
-    def test_param_name(self, request_to_server, request_to_db):
-        department = 'fullstack'
-        req = f'/api/department/?name={department}'
+    @pytest.mark.parametrize('dep', ['frontend', 'fullstack'])
+    def test_param_name(self, request_to_server, request_to_db, dep):
+        req = f'/api/department/?name={dep}'
         depart_list = list()
         for item in request_to_db:
-            if item['department'] not in depart_list and item['department'] == 'fullstack':
+            if item['department'] not in depart_list and item['department'] == f'{dep}':
                 depart_list.append(item['department'])
         assert request_to_server(req).get_json() == depart_list
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,19 @@
-def test_duplicate_id():
-    pass
+from datetime import datetime
 
 
-def test_null_data():
-    pass
+def test_duplicate_id(request_to_db):
+    temp_set = set()
+    for item in request_to_db:
+        temp_set.add(item['id'])
+    assert len(temp_set) == len(request_to_db)
 
 
-def test_date_format():
-    pass
+def test_null_data(request_to_db):
+    for item in request_to_db:
+        assert item['id'] and item['username'] and item['email'] and item['department'] and item['date_joined']
+
+
+def test_date_format(request_to_db):
+    for item in request_to_db:
+        assert datetime.strptime(item['date_joined'], '%Y-%m-%dT%H:%M:%S')
+
