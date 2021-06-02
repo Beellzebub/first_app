002d953e3082ba68e1b976f1061d688972ce59bf
Beellzebub <54277370+Beellzebub@users.noreply.github.com>
2021-06-03 00:31:20 +0500

    POST для users. Заготовки для PUT, DELETE.

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,8 @@ from app import app
 from app.users import users_list
 
 
-@app.route('/users/', methods=['GET'])
-def users():
-    """
-    Create endpoint /users contains list of users.
-    Filters (can used together):
-        -username (partial or complete match).
-        -department (complete match).
-    If matches not found - show empty page.
-    :return: users.html
-    """
+@app.route('/users', methods=['GET'])
+def get_users_html():
     username = request.args.get('username')
     depart_name = request.args.get('department')
     final_list = users_filter(username, depart_name)
@@ -20,66 +12,45 @@ def users():
 
 
 @app.route('/api/users/', methods=['GET'])
-def users_api():
-    """
-
-    :return: json
-    """
+def get_users_api():
     username = request.args.get('username')
     depart_name = request.args.get('department')
     final_list = users_filter(username, depart_name)
     return jsonify(final_list)
 
 
-@app.route('/department/', methods=['GET'])
-def department():
-    """
-    Create endpoint /department contains list of departments.
-    Filter:
-        -name (partial or complete match).
-    If matches not found - show empty page.
-    :return: departments.html
-    """
+@app.route('/api/users/', methods=['POST'])
+def post_users_api():
+    new_user = request.json
+    users_list.append(new_user)
+    return 'User successfully added.'
+
+
+@app.route('/api/users/<user_id>', methods=['PUT'])
+def put_users_api():
+    return 'Information about user successfully changed.'
+
+
+@app.route('/api/users/<user_id>', methods=['DELETE'])
+def del_users_api():
+    return 'User successfully deleted.'
+
+
+@app.route('/department', methods=['GET'])
+def get_department_html():
     depart_name = request.args.get('name')
     final_list = depart_filter(depart_name)
     return render_template('departments.html', title='Departments list', users=final_list)
 
 
 @app.route('/api/department/', methods=['GET'])
-def department_api():
-    """
-
-    :return:
-    """
+def get_department_api():
     depart_name = request.args.get('name')
     final_list = depart_filter(depart_name)
     return jsonify(final_list)
 
 
-def depart_filter(depart_name=None):
-    """
-
-    :param depart_name:
-    :return:
-    """
-    temp_list = list()
-    for user in users_list:
-        if depart_name:
-            if depart_name in user['department'] and user['department'] not in temp_list:
-                temp_list.append(user['department'])
-        else:
-            if user['department'] not in temp_list:
-                temp_list.append(user['department'])
-    return temp_list
-
-
 def users_filter(username=None, depart_name=None):
-    """
-
-    :param username:
-    :param depart_name:
-    :return:
-    """
     temp_list = list()
     for user in users_list:
         if username and depart_name:
@@ -93,4 +64,16 @@ def users_filter(username=None, depart_name=None):
                 temp_list.append(user)
         else:
             temp_list.append(user)
+    return temp_list
+
+
+def depart_filter(depart_name=None):
+    temp_list = list()
+    for user in users_list:
+        if depart_name:
+            if depart_name in user['department'] and user['department'] not in temp_list:
+                temp_list.append(user['department'])
+        else:
+            if user['department'] not in temp_list:
+                temp_list.append(user['department'])
     return temp_list
